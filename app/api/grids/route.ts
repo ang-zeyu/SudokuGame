185722231490b648f7d97a58379da12065e5f470
Ze Yu <angzeyu@gmail.com>
2024-07-20 12:23:54 +0800

    Better route handler error handling

--- app/api/grids/route.ts
+++ app/api/grids/route.ts
@@ -2,22 +2,29 @@ import { getGrids } from "@/api/grids";
 import {NextResponse} from "next/server";
 
 export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const gtId = searchParams.get('after')
-  const rawLimit = searchParams.get('limit');
-  if (!rawLimit) {
-    return NextResponse.json({ message: 'Missing limit' }, { status: 400 })
+  try {
+    const { searchParams } = new URL(request.url)
+    const gtId = searchParams.get('after')
+    const rawLimit = searchParams.get('limit');
+    if (!rawLimit) {
+      return NextResponse.json({ message: 'Missing limit' }, { status: 400 })
+    }
+
+    const limit = Number(rawLimit);
+    if (Number.isNaN(limit)) {
+      return NextResponse.json({ message: 'Non-numeric limit' }, { status: 400 })
+    } else if (limit > 100) {
+      return NextResponse.json({ message: 'Limit too large' }, { status: 400 })
+    }
+
+    const data = await getGrids({
+      gtId: gtId ? gtId : undefined,
+      limit,
+    })
+
+    return Response.json(data)
+  } catch (ex) {
+    console.error(ex);
+    return NextResponse.json({ message: 'Unknown error' }, { status: 500 })
   }
-
-  const limit = Number(rawLimit);
-  if (limit > 100) {
-    return NextResponse.json({ message: 'Limit too large' }, { status: 400 })
-  }
-
-  const data = await getGrids({
-    gtId: gtId ? gtId : undefined,
-    limit,
-  })
-
-  return Response.json(data)
 }
