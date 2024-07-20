7508fde97e5f095897c2cad9d2243e48bd0e3363
Ze Yu <angzeyu@gmail.com>
2024-07-20 10:38:26 +0800

    Remove unnecessary NEXT_PUBLIC exposure

--- utils/supabase/server.ts
+++ utils/supabase/server.ts
@@ -2,8 +2,8 @@ import { createServerClient } from "@supabase/ssr";
 
 export const createClient = () => {
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_URL!,
+    process.env.SUPABASE_ANON_KEY!,
     {
       cookies: {
         getAll() { return []; },
